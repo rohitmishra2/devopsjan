1bc874e626fa50174326e26ea231072c9947f600
Rohit Kumar <Jan1user210@rpsconsulting.in>
2026-01-28 11:24:29 +0530

    first change

---
+++ src/main/java/com/wip/MainClass.java
@@ -0,0 +1,9 @@
+package com.wip;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Java For Docker!!!! . THis code is pulled from WIP GitHub Repository");
+	}
+
+}
